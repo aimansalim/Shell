65407e59922ab822a6645674f2520fba11ea0ac4
aimansalim <61022549+aimansalim@users.noreply.github.com>
2022-04-12 21:58:55 +0200

    Update FCP.sh inside 28Mar2022

--- Esercitazioni/28Mar2022/Esercizio3/FCP.sh
+++ Esercitazioni/28Mar2022/Esercizio3/FCP.sh
@@ -24,7 +24,7 @@ if test $1 -eq 0
 fi
 
 #Il comando shift è utile per eliminare i parametri di una linea di comandi una volta analizzati.
-X=$1 #salviamo il primo parametro dopo aver fatto il controllo
+X=$1 #Salviamo il primo parametro dopo aver fatto il controllo
 #quindi ora possiamo usare il comando shift
 shift #toglie il primo parametro dai possibili successivi controlli
 
@@ -56,63 +56,9 @@ export PATH
 #Metto il risultato in un file tmp
  > /tmp/esercizio3$$ #creiamo/azzeriamo il file temporaneo. NOTA BENE: SOLO 1 file temporaneo!
 
-
-
-
-
-
-# #ora in $* abbiamo solo i nomi delle gerarchie e quindi possiamo fare i controlli sui nomi assoluti e sulle directory in un for
-# for i 
-# do
-# 	case $i in
-# 	/*) if test ! -d $i -o ! -x $i
-# 	    then
-# 	    echo $i non directory o non traversabile
-# 	    exit 4
-# 	    fi;;
-# 	*)  echo $i non nome assoluto; exit 5;;
-# 	esac
-# done
-
-# #controlli sui parametri finiti possiamo passare alle N fasi
-# PATH=`pwd`:$PATH
-# export PATH
-# > /tmp/conta$$ #creiamo/azzeriamo il file temporaneo. NOTA BENE: SOLO 1 file temporaneo!
-
-# for i
-# do
-# 	echo fase per $i 
-# 	#invochiamo il file comandi ricorsivo con la gerarchia, il numero e il file temporaneo
-# 	FCR.sh $i $X /tmp/conta$$   
-# done
-
-# #terminate tutte le ricerche ricorsive cioe' le N fasi
-# #N.B. Andiamo a contare le linee del file /tmp/conta$$
-# echo Il numero di file totali che soddisfano la specifica = `wc -l < /tmp/conta$$` 
-# for i in `cat /tmp/conta$$`
-# do
-# 	#Stampiamo (come richiesto dal testo) i nomi assoluti dei file trovati
-# 	echo Trovato il file $i
-# 	#chiediamo all'utente il numero K per ogni file trovato
-# 	echo -n "Dammi il numero K (strettamente maggiore di 0 e strettamente minore di $X): "
-# 	read K
-# 	#Controllo K (sempre con expr, se prima lo abbiamo fatto con expr, altrimenti sempre con case se prima lo abbiamo fatto con case!)
-# 	expr $K + 0  > /dev/null 2>&1
-# 	N1=$?
-# 	if test $N1 -ne 2 -a $N1 -ne 3
-# 	then 	#echo numerico $K siamo sicuri che numerico
-#      		if test $K -le 0 -o $K -ge $X 
-#      		then echo $K non positivo o non minore di $X 
-# 		rm /tmp/conta$$ #poiche' stiamo uscendo a causa di un errore, cancelliamo il file temporaneo!
-#           	exit 6
-#      	fi
-# 	else
-#   		echo $K non numerico
-# 		rm /tmp/conta$$ #poiche' stiamo uscendo a causa di un errore, cancelliamo il file temporaneo!
-#   		exit 7
-# 	fi
-# 	#selezioniamo direttamente la $K-esima linea del file corrente
-# 		head -$K $i | tail -1
-# done 
-# #da ultimo eliminiamo il file temporaneo
-# rm /tmp/conta$$
+for i
+do
+echo Fase per $i #La directory corrente
+#Invochiamo il file comandi ricorsivo con la Gerarchia ($i), il Numero ($X) e il File Temporaneo (/tmp/esercizio3$)
+FCR.sh $i $X /tmp/esercizio3$$
+done
